3b760ae61b11172fcd2fed07c9f4f7ddb53951e0
alduin10 <ryantomaztalavieira@gmail.com>
2024-11-21 11:50:30 -0300

    lista

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-let list = [3]
-document.getElementById(`list`).innerHTML = list.map(item => `<li>${item}</li>`).join('')
+let list = [2,12,212,213]
+let idbutton = document.querySelector("#button")
+let idinput = document.querySelector("#input")
+let classSaida = document.querySelector(".saida")
+
+idbutton.addEventListener("click", adiciona)
+
+function adiciona(){
+    let x = (idinput.value)
+    list += x
+    //classSaida.innerText = (list) 
+    document.querySelector("#list").innerHTML = list.map(item => `<li>${item}</li>`).join('')
+}
