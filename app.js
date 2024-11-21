4911a5d739b897a3d92f8cc7f86ba1ed5b092f65
alduin10 <ryantomaztalavieira@gmail.com>
2024-11-21 11:18:28 -0300

    lista
    21/11/24

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-let list = []
-document.getElementById(``).innerHTML = list.map(item => `<li>${item}<li>`).join('')
+let list = [2,5,3]
+document.getElementById(`list`).innerHTML = list.map(item => `<li>${item}<li>`).join('')
