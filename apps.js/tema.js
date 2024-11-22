ee904c3e08d30585ddb7ce01c957df00c2b6f154
talissonsidnei <158642399+MarlonUTF@users.noreply.github.com>
2024-11-22 15:32:49 -0300

    Adição de Tailwind e Modo dark

--- apps.js/tema.js
+++ apps.js/tema.js
@@ -1,13 +1,24 @@
 const themeToggle = document.querySelector(".modo");
 const themeIcon = document.querySelector("#theme-icon");
 
+// Verifica a preferência do tema no localStorage ao carregar a página
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.classList.add("dark");
+  themeIcon.src = "recursos/img/darkMode.svg";
+} else {
+  themeIcon.src = "recursos/img/lightMode.svg";
+}
+
 themeToggle.addEventListener("click", () => {
+  // Alterna a classe 'dark' no HTML
   document.documentElement.classList.toggle("dark");
 
-
+  // Atualiza o ícone e salva a preferência no localStorage
   if (document.documentElement.classList.contains("dark")) {
-    themeIcon.src = "recursos/img/lua.png";
+    themeIcon.src = "recursos/img/darkMode.svg";
+    localStorage.setItem("theme", "dark");
   } else {
-    themeIcon.src = "recursos/img/sol.svg";
+    themeIcon.src = "recursos/img/lightMode.svg";
+    localStorage.setItem("theme", "light");
   }
 });
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  darkMode: 'class', // Usando a classe 'dark' para alternar entre os modos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
