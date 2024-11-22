f148643042729defbe90c9f3eb52ee7645ac51df
talissonsidnei <158642399+MarlonUTF@users.noreply.github.com>
2024-11-22 11:41:15 -0300

    Modal

--- modal.js
+++ modal.js
@@ -1,11 +1,19 @@
-const closeModalButton = document.querySelector("#close-modal")
-const modal = document.querySelector("#modal")
-const fade = document.querySelector("#fade")
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.key.toLowerCase() === "h") {
+        event.preventDefault();
+        toggleModal();
+    }
+});
 
 const toggleModal = () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"))
-}
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
 
 [closeModalButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal())
-})
+  el.addEventListener("click", () => toggleModal());
+});
